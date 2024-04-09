d36e999b0ac5bd3853a178f77acba38425da6e15
Emma Bobola <emma@Emmas-MacBook-Air.local>
2024-04-08 20:08:41 -0700

    Add a comment to return statement

--- src/algorithms/parentheses_validation/validate_parentheses.py
+++ src/algorithms/parentheses_validation/validate_parentheses.py
@@ -2,15 +2,18 @@
 LeetCode prompt
 ref: https://leetcode.com/problems/valid-parenthesis-string/description/
 
-Given a string s containing only three types of characters: '(', ')' and '*', return true if s is valid.
+Given a string s containing only three types of characters: '(', ')' and '*',
+return true if s is valid.
 
 The following rules define a valid string:
 
 Any left parenthesis '(' must have a corresponding right parenthesis ')'.
 Any right parenthesis ')' must have a corresponding left parenthesis '('.
 Left parenthesis '(' must go before the corresponding right parenthesis ')'.
-'*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string "".
+'*' could be treated as a single right parenthesis ')' or a single left
+parenthesis '(' or an empty string "".
 """
+
 
 def parentheses_valid(string: str) -> bool:
     count = 0
@@ -30,4 +33,6 @@ def parentheses_valid(string: str) -> bool:
         elif c == "*":
             wildcards += 1
 
+    # if count is 0, all opens were explicitly closed
+    # if count is nonzero, but there are available wildcards, opens can be closed with wilds
     return count == 0 or (wildcards >= count)

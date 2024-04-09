399f26a43d98d57ae651b2967a657eb0c19a80fd
Emma Bobola <emma@Emmas-MacBook-Air.local>
2024-04-08 20:04:25 -0700

    with pre commit hooks gah

--- tests/unit/test_parentheses_validation.py
+++ tests/unit/test_parentheses_validation.py
@@ -1,9 +1,12 @@
-from src.algorithms.parentheses_validation.validate_parentheses import parentheses_valid
-
 import pytest
 
+from src.algorithms.parentheses_validation.validate_parentheses import parentheses_valid
 
-@pytest.mark.parametrize("input_string", [("()"), ("(*"), ("*)"), ("(**"), ("**)"), ("(*)"), ("(*))"), ("(())()")])
+
+@pytest.mark.parametrize(
+    "input_string",
+    [("()"), ("(*"), ("*)"), ("(**"), ("**)"), ("(*)"), ("(*))"), ("(())()")],
+)
 def test_valid_parentheses(input_string: str):
     assert parentheses_valid(input_string)
 

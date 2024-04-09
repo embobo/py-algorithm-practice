cdebcd4ac9db584785422491be8b067decdb822f
Emma Bobola <emma@Emmas-MacBook-Air.local>
2024-04-08 19:42:24 -0700

    Add method and first unit test
    test calls and (correctly) fails

---
+++ tests/unit/test_parentheses_validation.py
@@ -0,0 +1,8 @@
+from src.algorithms.parentheses_validation.validate_parentheses import parentheses_valid
+
+import pytest
+
+
+@pytest.mark.parametrize("input_string", [("()")])
+def test_valid_parentheses(input_string: str):
+    assert parentheses_valid(input_string)

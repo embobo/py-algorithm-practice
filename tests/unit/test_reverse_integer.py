4e9261dcd016fa15320a948f7f3f4e3eb698fa34
Emma Bobola <embobola@gmail.com>
2024-06-10 15:01:54 -0400

    Typo

--- tests/unit/test_reverse_integer.py
+++ tests/unit/test_reverse_integer.py
@@ -3,7 +3,7 @@ import pytest
 from src.algorithms.reverse_integer import reverse_integer
 
 
-class TestCountSubstrings:
+class TestReverseInteger:
     @pytest.mark.parametrize(
         "in_int, expected",
         [(6, 6), (123, 321), (-321, -123), (2147483648, 0)],
